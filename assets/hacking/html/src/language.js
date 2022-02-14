eef7e782994d7a4f4cc585295db6ead3370b8b2a
SocialPeely <nocityroleplay@gmail.com>
2022-02-14 14:52:12 +0000

    hacking translate

---
+++ assets/hacking/html/src/language.js
@@ -0,0 +1,26 @@
+export default
+{
+    // langue française = 'FR'
+    // lengua española = 'ES'
+    // lingua italiana = 'IT'
+    // english language = 'EN'
+    // svenska språk = 'SE'
+    SELECTED_LANGUAGE : 'EN',
+
+    LANGUAGES : ['EN', 'ES', 'IT', 'FR', 'SE'],
+    EN : {
+        COLORS : ['black', 'white', 'blue', 'red', 'yellow', 'orange', 'green', 'purple']
+    },
+    ES : {
+        COLORS : ['negro', 'blanco', 'azul', 'rojo', 'amarillo', 'naranja', 'verde', 'púrpura']
+    },
+    IT : {
+        COLORS : ['nero', 'bianco', 'blu', 'rosso', 'giallo', 'arancione', 'verde', 'viola']
+    },
+    FR : {
+        COLORS : ['noir', 'blanc', 'bleu', 'rouge', 'jaune', 'orange', 'vert', 'violet']
+    },
+    SE : {
+        COLORS : ['svart', 'vit', 'blå', 'röd', 'gul', 'orange', 'grön', 'lila']
+    }
+}
